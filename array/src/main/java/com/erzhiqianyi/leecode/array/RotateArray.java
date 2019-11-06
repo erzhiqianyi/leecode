4f8dda1e4f77c7629aaa47f4cabf0d5b044011d5
二之前一 <github@caofeng.me>
2019-11-06 08:50:32 +0800

    189. Rotate Array

---
+++ array/src/main/java/com/erzhiqianyi/leecode/array/RotateArray.java
@@ -0,0 +1,54 @@
+package com.erzhiqianyi.leecode.array;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+/**
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * Example 2:
+ * <p>
+ * Input: [-1,-100,399] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * Note:
+ * <p>
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * <p>
+ * time: 7:35 - 2019/11/4
+ *
+ * @author 二之前一
+ */
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverseArray(nums,0,nums.length-1);
+        reverseArray(nums,0,k-1);
+        reverseArray(nums,k,nums.length-1);
+
+    }
+
+    public void reverseArray(int[] nums ,int i ,int j ){
+        int temp;
+        while ( i < j){
+           temp= nums[i] ;
+           nums[i] = nums[j];
+           nums[j] =temp;
+           i++;
+           j--;
+        }
+    }
+
+
+}
---
+++ array/src/test/java/com/erzhiqianyi/leecode/array/RotateArrayTest.java
@@ -0,0 +1,116 @@
+package com.erzhiqianyi.leecode.array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RotateArrayTest {
+
+    private RotateArray rotateArray;
+
+    @Before
+    public void init() {
+        rotateArray = new RotateArray();
+    }
+
+    @Test
+    public void rotate() {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        int[] expected = new int[]{5, 6, 7, 1, 2, 3, 4};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotateOne() {
+        int[] nums = new int[]{1};
+        int k = 1;
+        int[] expected = new int[]{1};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotateTwo() {
+        int[] nums = new int[]{1, 2};
+        int k = 1;
+        int[] expected = new int[]{2, 1};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotateThree() {
+        int[] nums = new int[]{1, 2};
+        int k = 3;
+        int[] expected = new int[]{2, 1};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotateFour() {
+        int[] nums = new int[]{-1};
+        int k = 0;
+        int[] expected = new int[]{-1};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotateFive() {
+        int[] nums = new int[]{1, 2, 3};
+        int k = 2;
+        int[] expected = new int[]{2, 3, 1};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotateSix() {
+        int[] nums = new int[]{-1, -100, 3, 99};
+        int k = 3;
+        int[] expected = new int[]{-100, 3, 99, -1};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+
+    }
+
+    @Test
+    public void rotateSeven() {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        int k = 0;
+        int[] expected = new int[]{1, 2, 3, 4, 5, 6};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6};
+        k = 1;
+        expected = new int[]{6, 1, 2, 3, 4, 5};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6};
+        k = 2;
+        expected = new int[]{5,6, 1, 2, 3, 4};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6};
+        k = 3;
+        expected = new int[]{4,5,6, 1, 2, 3};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6};
+        k = 4;
+        expected = new int[]{3,4,5,6, 1, 2};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+
+
+
+    }
+}
